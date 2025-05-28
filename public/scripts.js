45f7a5b75f5bab35c2535d06e2983e15d71ef037
timkim <timk@adobe.com>
2025-05-28 15:55:25 -0700

    Add in the project

---
+++ public/scripts.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", (event) => {
+  console.log("page is fully loaded");
+  const main = document.querySelector('div#main');
+  console.log(main);
+
+  const announce = document.querySelector('div.announce');
+  console.log(announce);
+
+  const form = document.querySelector('div#formExmaple');
+  console.log(form);
+});
