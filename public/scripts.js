2bd5dfb22d478d5c1ef785d9abc95f08488e4a04
timkim <timk@adobe.com>
2025-05-30 12:26:40 -0700

    Add in createTag function

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,13 @@
+function createTag(name, attrs) {
+  const el = document.createElement(name);
+  if (typeof attrs === 'object') {
+    Object.entries(attrs).forEach(([key, value]) => {
+      el.setAttribute(key, value);
+    });
+  }
+  return el;
+}
+
 window.addEventListener("load", (event) => {
   console.log("page is fully loaded");
   const main = document.querySelector('div#main');
@@ -8,4 +18,10 @@ window.addEventListener("load", (event) => {
 
   const form = document.querySelector('div#formExmaple');
   console.log(form);
+
+  const divContainer = createTag('div', {class: 'container'});
+  main.append(divContainer);
+
+  const listContainer = createTag('li');
+
 });
